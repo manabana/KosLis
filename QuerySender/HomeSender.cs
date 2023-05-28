ed36e62f7272fd4d8fdd86703bd462c17f4de500
Kost mans <mansurkostoev679@gmail.com>
2023-05-28 04:03:34 +0300

    Идет работа над удалением постов

--- QuerySender/HomeSender.cs
+++ QuerySender/HomeSender.cs
@@ -593,7 +593,7 @@ namespace QuerySender
         }
         public static string GetSome1Info(int userId)
         {
-            byte[] bytes = new byte[256];
+            byte[] bytes = new byte[512];
             IPHostEntry ipHost = Dns.GetHostEntry("localhost");
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
@@ -602,6 +602,54 @@ namespace QuerySender
             {
                 sender.Connect(ipEndPoint);
                 string message = $"userLimitedInfo;{userId}";
+                byte[] msg = Encoding.UTF8.GetBytes(message);
+                int bytesSent = sender.Send(msg);
+                int bytesRec = sender.Receive(bytes);
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                return Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                return "ServerNotResponding";
+            }
+
+        }
+        public static string DeletePost(int postid, string userPassword)
+        {
+            byte[] bytes = new byte[32];
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sender.Connect(ipEndPoint);
+                string message = $"deletePost;{postid};{userPassword}";
+                byte[] msg = Encoding.UTF8.GetBytes(message);
+                int bytesSent = sender.Send(msg);
+                int bytesRec = sender.Receive(bytes);
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                return Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                return "ServerNotResponding";
+            }
+
+        }
+        public static string EditUser(string type, int userid, string text, string password)
+        {
+            byte[] bytes = new byte[32];
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 11000);
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                sender.Connect(ipEndPoint);
+                string message = $"editUser;{type};{userid};{text};{password}";
                 byte[] msg = Encoding.UTF8.GetBytes(message);
                 int bytesSent = sender.Send(msg);
                 int bytesRec = sender.Receive(bytes);
